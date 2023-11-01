655e4ecf0415bcd074aa72bd754c7ae9aa6170ef
Tuan Ha <hatuan@email.cz>
2023-11-01 12:30:14 +0100

    add basic calculator functions to js

---
+++ js/script.js
@@ -0,0 +1,40 @@
+//variables for 2 numbers and an operator
+let firstNumber;
+let secondNumber;
+let operator;
+
+// Functions for add, substract, multiply, divide
+function add(a, b) {
+    return a + b;
+}
+
+function substract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return (a / b).toFixed(4);
+}
+
+// a function the creates an operation
+function operate(a, operator, b) {
+    let result = 0;
+
+    if (operator === "+") {
+        result = add(a, b);
+    } else if (operator === "-") {
+        result = substract(a, b);
+    } else if (operator === "*") {
+        result = multiply(a, b);
+    } else if (operator === "/") {
+        result = divide(a, b);
+    } else {
+        return "ERROR";
+    }
+
+    return result;
+}
